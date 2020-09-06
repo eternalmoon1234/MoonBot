394945e0c826c96635c5439ea57a5574b69ad8e3
EternalizedMoonlight <eternal@MacBook-Pro.local>
2020-09-05 18:36:38 -0700

    Client.js structure

---
+++ index.js
@@ -0,0 +1,4 @@
+const BotClient = require('./structures/Client')
+const newClient = new BotClient()
+
+newClient.initBot()
--- structures/Client.js
+++ structures/Client.js
@@ -1,1 +1,63 @@
 const { Collection, Client } = require('discord.js')
+const { token, prefix } = require('../config.json')
+
+class BotClient extends Client {
+    constructor() {
+        super()
+
+        //Create Collection data structures
+        this.commands = new Collection()
+        this.aliases = new Collection()
+
+        //Require Modules
+        this.fs = require('fs')
+        this.Discord = require('discord.js')
+        this.path = require('path')
+    }
+
+    //Functions of the class
+    commandHandler() {
+        this.fs.readdirSync('./Commands').map((directory) => {
+            const Files = this.fs.readdirSync(`./Commands/${directory}/`).map((cmd) => {
+                let CMD = require(`../Commands/${directory}/${cmd}`)
+                console.log(`Command ${CMD.name} loaded`)
+
+                this.commands.set(CMD.name, CMD)
+            })
+        })
+    }
+
+    initBot() {
+        this.commandHandler()
+
+        //Ready event listener
+        this.on('ready', () => {
+            console.log(`Client connected as ${this.user.tag}`)
+        })
+
+        //Message event listener
+        this.on('message', message => {
+            //If the message comes from another bot, then return
+            if (message.author.bot) {
+                return;
+            }
+
+            //Variable for args
+            const args = message.content.slice(prefix.length).trim().split(/ +/)
+            const lower = args.shift().toLowerCase()
+
+            //Navigate the collection, get, and run
+            if (this.commands.has(lower)) {
+                const commandFiles = this.commands.get(lower)
+                //Run files in directories
+                commandFiles.run(this, message, args)
+            }
+        })
+
+        //Login
+        this.login(token)
+    }
+}
+
+//Export the class
+module.exports = BotClient;
