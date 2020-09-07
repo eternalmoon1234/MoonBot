9690de03f85257bc16461a96c0845dd2dbca4dd6
EternalizedMoonlight <eternal@MacBook-Pro.local>
2020-09-06 19:45:27 -0700

    Ping + Avatar Command

---
+++ Commands/misc/Ping.js
@@ -0,0 +1,27 @@
+const Client = require('../../structures/Client')
+const { Message, MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'ping',
+    description: 'returns the websocket ping, and message edit ping',
+    category: 'misc',
+
+    //IntelliSense
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     */
+
+    run: async(client, message, args) => {
+        const msg = await message.channel.send('🏓 Pinging... 🏓')
+        //Create an embed by making an instance of the MessageEmbed class, + set properties
+        const ping = new MessageEmbed()
+        ping.setTitle('🏓 Pong! 🏓')
+        ping.setDescription(`WebSocket Ping: ${client.ws.ping} MS | Edit Ping: ${msg.createdAt - message.createdAt} MS`)
+        ping.setColor(`4CCD44`)
+        ping.setFooter(`Requested by ${message.author.tag}`)
+        //Using the await function to use a var we can use again
+        await msg.edit(ping)
+        await msg.edit("🏓 Pong! 🏓")
+    }
+}
---
+++ Commands/misc/avatar.js
@@ -0,0 +1,36 @@
+const { MessageEmbed, Message } = require("discord.js")
+const Client = require('../../structures/Client')
+
+module.exports = {
+    name: 'avatar',
+    description: 'returns the avatar',
+    category: "misc",
+    
+    //IntelliSense
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     * @param {String[]} args
+     */
+
+    run: async(client, message, args) => {
+        let mentioneduser = message.mentions.users.first()
+
+        //Your avatar
+        if (!mentioneduser) {
+            const youravatar = new MessageEmbed()
+            youravatar.setTitle('Your Avatar')
+            youravatar.setColor(`RANDOM`)
+            youravatar.setThumbnail(message.author.displayAvatarURL())
+            youravatar.setFooter(`Requested by ${message.author.tag}`)
+        message.channel.send(youravatar)
+        } else {
+            const mentionavatar = new MessageEmbed()
+            mentionavatar.setTitle(`${mentioneduser.username}'s Avatar`)
+            mentionavatar.setThumbnail(mentioneduser.displayAvatarURL())
+            mentionavatar.setColor(`RANDOM`)
+            mentionavatar.setFooter(`Requested by ${message.author.tag}`)
+            message.channel.send(mentionavatar)
+        }
+    }
+}
