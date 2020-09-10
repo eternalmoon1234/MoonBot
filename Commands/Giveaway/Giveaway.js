764ba91fed32e447e52d7a7767d28ed9c43edffd
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2020-09-10 09:45:24 -0700

    Minor changes, userinfo, serverinfo

---
+++ Commands/Giveaway/Giveaway.js
@@ -0,0 +1,19 @@
+const Client = require('../../structures/Client')
+const { Message, MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'giveaway',
+    description: 'make a giveaway',
+    category: 'Giveaway',
+
+    //IntelliSense
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     * @param {String[]} args
+     */
+
+    run: async(client, message, args) => {
+        
+    }
+}
---
+++ Commands/info/ServerInfo.js
@@ -0,0 +1,19 @@
+const Client = require('../../structures/Client')
+const { Message, MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'serverinfo',
+    description: 'serverinfo of the guild',
+    category: 'info',
+
+    //IntelliSense
+    /**
+     * @param {Message} message
+     * @param {Client} client
+     * @param {String[]} args
+     */
+
+    run: async(client, message, args) => {
+        
+    }
+}
